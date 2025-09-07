02c1b8bf72cfc448bf871428cf8d8994e80cbbe0
Terezsak Jozsef <jozseft@digitalarcher.dev>
2025-09-07 20:53:51 +0200

    Fix: frontend setup in docker, and proper playwright installation

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -6,11 +6,11 @@ export default defineConfig({
   workers: 4,
   retries: 0,
   use: {
-    baseURL: 'http://localhost:3000',
+    baseURL: process.env.PLAYWRIGHT_BASE_URL || 'http://localhost:3000',
     headless: true,
     trace: 'on-first-retry',
   },
-  webServer: {
+  webServer: process.env.DOCKER_ENV ? undefined : {
     command: 'npm run dev',
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
@@ -18,7 +18,13 @@ export default defineConfig({
   projects: [
     {
       name: 'Chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { 
+        ...devices['Desktop Chrome'],
+        launchOptions: {
+          executablePath: process.env.PLAYWRIGHT_CHROMIUM_EXECUTABLE_PATH || undefined,
+          args: process.env.DOCKER_ENV ? ['--no-sandbox', '--disable-setuid-sandbox'] : undefined
+        }
+      },
     }
   ],
 });
